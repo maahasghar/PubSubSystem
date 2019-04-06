94c50b17ccaafea0b0779b858bcd4ed6e099ce54
maahasghar <49345824+maahasghar@users.noreply.github.com>
2019-04-06 13:30:41 -0400

    Create AStat.java

---
+++ PubSubSystem/src/states/subscriber/AStat.java
@@ -0,0 +1,14 @@
+/**@author Maah Asghar*/
+
+package states.subscriber;
+
+import events.AbstractEvent;
+
+public class AState implements IState {
+	
+	public void handleEvent(AbstractEvent event, String channelName) {
+		System.out.println(event + "has been handled on" + channelName);
+		
+	}
+
+}
